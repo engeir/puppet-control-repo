3cd8b6a8e6a956a91c5a885eaf8a8772d8c61660
Eirik Rolland Enger <eirik.enger@nhn.no>
2025-08-13 12:28:53 +0200

    fix: make sure ror-cli is on PATH

--- site-modules/profile/manifests/rorclitest.pp
+++ site-modules/profile/manifests/rorclitest.pp
@@ -1,6 +1,6 @@
 # ROR-CLI repo
 class profile::rorclitest (
-    Variant[String[1],Integer[0,59]] $cron_minute = '*',
+    Variant[String[1],Integer[0,59]] $cron_minute = '0',
     Variant[String[1],Integer[0,59]] $cron_hour = '*',
 ){
     file { '/usr/local/sbin/git-sync-ror-cli':
@@ -10,6 +10,18 @@ class profile::rorclitest (
         mode => '0755',
         content => epp('profile/git-sync-ror-cli.sh.epp', { install_dir => '/usr/local/bin' }),
     }
+
+    file { '/etc/profile.d/localbin_path.sh':
+        ensure  => 'file',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => @(EOT)
+if ! echo "$PATH" | grep -q "/usr/local/bin"; then
+  export PATH="/usr/local/bin:$PATH"
+fi
+        | EOT
+        }
 
     cron { 'git-sync-ror-cli':
         ensure =>'present',
