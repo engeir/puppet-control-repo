8ea8cde30e343dfd266c27581a35d7d3d01f1b76
Eirik Rolland Enger <eirik.enger@nhn.no>
2025-08-26 20:47:44 +0200

    chore: delete test files for node specific config

--- site/profile/manifests/basetest2.pp
+++
@@ -1,6 +0,0 @@
-class profile::basetest2 {
-  file { '/etc/basetest2.txt':
-    ensure  => present,
-    content => lookup('basetest2_content', {default_value => 'default content from class'}),
-  }
-}
--- site/role/manifests/basic.pp
+++ site/role/manifests/basic.pp
@@ -1,5 +1,4 @@
 class role::basic {
   include profile::basetest
-  include profile::basetest2
   include profile::rorclitest
 }
